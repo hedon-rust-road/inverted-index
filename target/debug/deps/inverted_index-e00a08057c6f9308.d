4f210cb58375280eefb50498e1bffea3992f5d32
hedon954 <171725713@qq.com>
2024-04-21 23:46:20 +0800

    update Cargo.tml lisence

--- target/debug/deps/inverted_index-e00a08057c6f9308.d
+++
@@ -1,5 +0,0 @@
-/Users/hedon/RustActions/rust-action-by-hedon/inverted_index/target/debug/deps/libinverted_index-e00a08057c6f9308.rmeta: src/main.rs
-
-/Users/hedon/RustActions/rust-action-by-hedon/inverted_index/target/debug/deps/inverted_index-e00a08057c6f9308.d: src/main.rs
-
-src/main.rs:
